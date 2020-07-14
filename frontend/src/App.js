08542360eac58fa86e55d8a4f1261f6b6b80da7b
Shain Dholakiya <shainshain99@gmail.com>
2020-07-14 02:56:27 -0400

    added search and pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,16 @@ import axios from "axios";
 import "./App.css";
 
 import React from "react";
-import { useTable } from "react-table";
+// import { useTable } from "react-table";
 import styled from "styled-components";
+import { useTable, useFilters, useGlobalFilter, useAsyncDebounce, usePagination, useBlockLayout, useFlexLayout } from 'react-table'
+import { FixedSizeList } from 'react-window'
+
+import matchSorter from 'match-sorter'
 
 const Styles = styled.div`
+padding: 1rem;
+
   table {
     width: 100%;
     border-spacing: 0;
@@ -20,6 +26,7 @@ const Styles = styled.div`
     }
     th,
     td {
+      font-size: 12px;
       margin: 0;
       padding: 1rem;
       border-bottom: 1px solid black;
@@ -44,42 +51,346 @@ Object.filter = function (obj, prop) {
   return result;
 };
 
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
 function Table({ columns, data }) {
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+  
+  // Use the state and functions returned from useTable to build your UI
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
     prepareRow,
-  } = useTable({ columns, data });
+    page, // Instead of using 'rows', we'll use page,
+    // which has only the rows for the active page
+    rows,
+    totalColumnsWidth,
 
-  // Render Data Table UI
+    // The rest of these things are super handy, too ;)
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+
+    // filter
+    preGlobalFilteredRows,
+    setGlobalFilter,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 },
+      defaultColumn, // Be sure to pass the defaultColumn option
+      filterTypes
+    },
+    usePagination,
+    // useFlexLayout,
+    // useBlockLayout,
+    useFilters, // useFilters!
+    useGlobalFilter // useGlobalFilter!
+  )
+
+  const RenderRow = React.useCallback(
+    ({ index, style }) => {
+      const row = rows[index]
+      prepareRow(row)
+      return (
+        <div
+          {...row.getRowProps({
+            style,
+          })}
+          className="tr"
+        >
+          {row.cells.map(cell => {
+            return (
+              <div {...cell.getCellProps()} className="td">
+                {cell.render('Cell')}
+              </div>
+            )
+          })}
+        </div>
+      )
+    },
+    [prepareRow, rows]
+  )
+
+  // Render the UI for your table
   return (
-    <table {...getTableProps()} className='table-light table-responsive'>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
-              })}
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
+                </th>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+
+      {/* <div {...getTableBodyProps()}>
+        <FixedSizeList
+          height={400}
+          itemCount={page.length}
+          itemSize={35}
+          width={totalColumnsWidth}
+        >
+          {RenderRow}
+        </FixedSizeList>
+      </div> */}
+
+      </table>
+      <br />
+      {/* <div>Showing the first 10 results of {page.length} rows</div> */}
+      {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+      <div className="pagination">
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {'<'}
+        </button>{' '}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {'>'}
+        </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(page)
+            }}
+            style={{ width: '100px' }}
+          />
+        </span>{' '}
+        <select
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  )
 }
+
+// function Table({ columns, data }) {
+//   const {
+//     getTableProps,
+//     getTableBodyProps,
+//     headerGroups,
+//     rows,
+//     prepareRow,
+//   } = useTable({ columns, data });
+
+//   // Render Data Table UI
+//   return (
+//     <table {...getTableProps()} className='table-light table-responsive'>
+//       <thead>
+//         {headerGroups.map((headerGroup) => (
+//           <tr {...headerGroup.getHeaderGroupProps()}>
+//             {headerGroup.headers.map((column) => (
+//               <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+//             ))}
+//           </tr>
+//         ))}
+//       </thead>
+//       <tbody {...getTableBodyProps()}>
+//         {rows.map((row, i) => {
+//           prepareRow(row);
+//           return (
+//             <tr {...row.getRowProps()}>
+//               {row.cells.map((cell) => {
+//                 return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+//               })}
+//             </tr>
+//           );
+//         })}
+//       </tbody>
+//     </table>
+//   );
+// }
+
+
+// // Our table component
+// function Table({ columns, data }) {
+//   const filterTypes = React.useMemo(
+//     () => ({
+//       // Add a new fuzzyTextFilterFn filter type.
+//       fuzzyText: fuzzyTextFilterFn,
+//       // Or, override the default text filter to use
+//       // "startWith"
+//       text: (rows, id, filterValue) => {
+//         return rows.filter(row => {
+//           const rowValue = row.values[id]
+//           return rowValue !== undefined
+//             ? String(rowValue)
+//                 .toLowerCase()
+//                 .startsWith(String(filterValue).toLowerCase())
+//             : true
+//         })
+//       },
+//     }),
+//     []
+//   )
+
+//   const defaultColumn = React.useMemo(
+//     () => ({
+//       // Let's set up our default Filter UI
+//       Filter: DefaultColumnFilter,
+//     }),
+//     []
+//   )
+
+//   const {
+//     getTableProps,
+//     getTableBodyProps,
+//     headerGroups,
+//     rows,
+//     prepareRow,
+//     state,
+//     visibleColumns,
+//     preGlobalFilteredRows,
+//     setGlobalFilter,
+//   } = useTable(
+//     {
+//       columns,
+//       data,
+//       defaultColumn, // Be sure to pass the defaultColumn option
+//       filterTypes,
+//     },
+//     useFilters, // useFilters!
+//     useGlobalFilter // useGlobalFilter!
+//   )
+
+//   // We don't want to render all of the rows for this example, so cap
+//   // it for this use case
+//   const firstPageRows = rows.slice(0, 10)
+
+//   return (
+//     <>
+//       <table {...getTableProps()}>
+//         <thead>
+//           {headerGroups.map(headerGroup => (
+//             <tr {...headerGroup.getHeaderGroupProps()}>
+//               {headerGroup.headers.map(column => (
+//                 <th {...column.getHeaderProps()}>
+//                   {column.render('Header')}
+//                   {/* Render the columns filter UI */}
+//                   <div>{column.canFilter ? column.render('Filter') : null}</div>
+//                 </th>
+//               ))}
+//             </tr>
+//           ))}
+//         </thead>
+//         <tbody {...getTableBodyProps()}>
+//           {firstPageRows.map((row, i) => {
+//             prepareRow(row)
+//             return (
+//               <tr {...row.getRowProps()}>
+//                 {row.cells.map(cell => {
+//                   return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+//                 })}
+//               </tr>
+//             )
+//           })}
+//         </tbody>
+//       </table>
+//       <br />
+//       <div>Showing the first 10 results of {rows.length} rows</div>
+//     </>
+//   )
+// }
+
+
 
 export default class App extends React.Component {
   constructor(props) {
