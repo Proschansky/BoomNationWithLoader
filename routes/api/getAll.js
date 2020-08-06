1637ddba71bf8b435d6bf7bb8c64caf34b838db1
Proschansky <Samproschansky@gmail.com>
2020-08-05 20:33:12 -0400

    Added a getAll route to return the entire database.

---
+++ routes/api/getAll.js
@@ -0,0 +1,18 @@
+const db = require("../../models");
+
+module.exports = async (req, res)=>{
+    let jobListings;
+    await db.Manufacturing.find().then(res => jobListings = res)
+    .then( await db.Petrochemicals.find().then(res=>{
+        jobListings = jobListings.concat(res)
+    }))
+    .then(await db.Trucking.find().then(res=>{
+        jobListings = jobListings.concat(res)
+    }))
+    .then(await db.OilAndGas.find().then(res=>{
+        jobListings = jobListings.concat(res)
+    }))
+
+    res.json(jobListings);
+    
+};
